36bdd96b0049d618e5cf9d0ca96cc192715dfc7d
olanikegloria github <olanikegloria2020@gmail.com>
2023-07-06 20:00:07 +0100

    fetch quotes

---
+++ src/components/quotes.js
@@ -0,0 +1,47 @@
+import './calculator.css';
+import { useState, useEffect } from 'react';
+
+function Quotes() {
+  const [quote, setquote] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness',
+          {
+            headers: {
+              'X-Api-Key': 'Y4AovPhNanOmDoemBuILsg==Vh1LebLXqqcrnzL3',
+            },
+          });
+        const data = await res.json();
+        console.log(data);
+        setquote(data);
+      } catch (error) {
+        setHasError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [setquote, setLoading]);
+  if (hasError) return <div className="container-error"> something went wrong</div>;
+  if (loading) return <div className="container-loading"> loading...</div>;
+
+  return (
+    <div className="quotes-container">
+      {quote.map((item) => (
+        <div key={item.quote} className="quote">
+          {item.quote}
+          <p>
+            <span> by </span>
+            {item.author}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Quotes;
