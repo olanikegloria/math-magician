3ea60d9202844f9120299cc615356b44ab171e68
olanikegloria github <olanikegloria2020@gmail.com>
2023-07-11 22:54:00 +0100

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quotes from './components/quotes';
+import Home from './components/home';
+import Layout from './components/layout';
 
 function App() {
   return (
     <div className="app">
-      <Quotes />
-      <Calculator />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quotes" element={<Quotes />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
